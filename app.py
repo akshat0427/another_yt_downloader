5f71ac85e0067bcf25902463a3739676cf4e5942
akshat <akshatkabra91@gmail.com>
2024-05-14 23:22:14 +0530

    v. 3

--- app.py
+++ app.py
@@ -1,21 +1,51 @@
 ''' to get the history of your downloads open links.pkl '''
 
 from pytube import YouTube
+import re
 import tkinter as tk
 from tkinter import filedialog
 import pickle
+from datetime import datetime
+
+def remove_special_characters(text):
+    pattern = r'[^a-zA-Z0-9\s]'
+    clean_text = re.sub(pattern, '', text)
+    
+    return clean_text
+
+
+def read_path():    
+    with open("./directory.pkl", "rb") as file:    
+        unpickled_string = pickle.loads(file.read())
+        return unpickled_string
+
 
 def get_video(lnk, n):
+    
+       
     if n == 1:   
+        
+        
         yt = YouTube(lnk)   
-        title= yt.title
+        title= remove_special_characters(yt.title)+".mp4"
     
         print("downloading ", title)    
         stream = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
 
     # Download the video
-        stream.download(filename=f"{title}.mp4", output_path=read_path())    
-        print("downloaded ", title, "\n")    
+        try:
+            
+            # print(title)
+            
+            # print(f"{read_path()}/{title}.mp4")
+    
+            stream.download(filename=title, output_path=read_path())    
+            print("downloaded ", title, "\n")    
+        except Exception as e1:
+            stream.download(filename=f"{str(title)}.mp4", output_path=read_path())    
+            print("downloaded ", title, "\n")    
+
+            
     else:
         c= 0 
         for i in lnk:
@@ -25,6 +55,7 @@ def get_video(lnk, n):
             title= yt.title
         
             print("downloading ",c, title)    
+            
             stream = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
 
         # Download the video
@@ -32,7 +63,6 @@ def get_video(lnk, n):
             print("downloaded ", c,title, "\n")    
         
 
-from datetime import datetime
 
 # # Example data
 
@@ -53,10 +83,6 @@ def store_url(p1):
     
         
     
-def read_path():    
-    with open("./directory.pkl", "rb") as file:    
-        unpickled_string = pickle.loads(file.read())
-        return unpickled_string
  
         
 def store_path(p):   
@@ -82,21 +108,24 @@ def get_directory_path():
 # d_path = get_directory_path()
 # store_path(d_path)
 
+    
+
+
 urls = []
 try:
     qty = int(input("number of videos-     -"))
     if qty == 1:
         link = input("enter youtube url-    -")
         # store the video url - 
-        store_url(link)
         get_video(link, qty)
+        # store_url(link)
     else:
         for i in range(qty):
             
             link = input("enter youtube url-    -")
             urls.append(link)
             
-        store_url(urls)
         get_video(urls, qty)
+        # store_url(urls)
     
-except Exception as e:     print("you entered something wrong ")
+except Exception as e:     print("you entered something wrong ", e)
